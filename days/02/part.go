7ad42cbf10ae9dc07080b13d45827377bf2ff307
Raphael Pour <info@raphaelpour.de>
2024-12-02 12:55:29 +0100

    02: outsource to stellar

--- days/02/part.go
+++ days/02/part.go
@@ -2,56 +2,16 @@ package main
 
 import (
 	"fmt"
-	"iter"
-	"slices"
 
+	"github.com/RaphaelPour/stellar/generator"
 	"github.com/RaphaelPour/stellar/input"
+	"github.com/RaphaelPour/stellar/math"
 )
-
-func SkipOne[S ~[]T, T any](in S) []S {
-	return slices.Collect(SkipOneSeq(in))
-}
-
-func SkipOneSeq[S ~[]T, T any](in S) iter.Seq[S] {
-	return func(yield func(S) bool) {
-		out := make(S, len(in)-1)
-		for i := 0; i < len(in); i += 1 {
-			copy(out[:i], in[:i])
-			copy(out[i:], in[i+1:])
-			if !yield(out) {
-				return
-			}
-		}
-	}
-}
-
-func SkipOneSeq2[S ~[]T, T any](in S) iter.Seq2[int, S] {
-	return func(yield func(int, S) bool) {
-		out := make(S, len(in)-1)
-		for i := 0; i < len(in); i += 1 {
-			copy(out[:i], in[:i])
-			copy(out[i:], in[i+1:])
-			if !yield(i, out) {
-				return
-			}
-		}
-	}
-}
-
-func isMonotonic(report []int, minDiff, maxDiff int) bool {
-	for i := 0; i < len(report)-1; i += 1 {
-		diff := report[i+1] - report[i]
-		if diff < minDiff || diff > maxDiff {
-			return false
-		}
-	}
-	return true
-}
 
 func part1(data [][]int) int {
 	safe := 0
 	for _, line := range data {
-		if isMonotonic(line, 1, 3) || isMonotonic(line, -3, -1) {
+		if math.IsMonotonicWithinRange(line, 1, 3) || math.IsMonotonicWithinRange(line, -3, -1) {
 			safe += 1
 		}
 	}
@@ -61,14 +21,13 @@ func part1(data [][]int) int {
 func part2(data [][]int) int {
 	safe := 0
 	for _, line := range data {
-		if isMonotonic(line, 1, 3) || isMonotonic(line, -3, -1) {
+		if math.IsMonotonicWithinRange(line, 1, 3) || math.IsMonotonicWithinRange(line, -3, -1) {
 			safe += 1
 			continue
 		}
 
-		//https://go.dev/play/p/-k4lZnPMSgg
-		for line2 := range SkipOneSeq(line) {
-			if isMonotonic(line2, 1, 3) || isMonotonic(line2, -3, -1) {
+		for line2 := range generator.SkipOneSeq(line) {
+			if math.IsMonotonicWithinRange(line2, 1, 3) || math.IsMonotonicWithinRange(line2, -3, -1) {
 				safe += 1
 				break
 			}
@@ -78,9 +37,6 @@ func part2(data [][]int) int {
 }
 
 func main() {
-	// data := input.LoadString("input")
-	// data := input.LoadDefaultInt()
-	// data := input.LoadInt("input")
 	data := input.LoadIntTable("input")
 
 	fmt.Println("== [ PART 1 ] ==")
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.23
 
 require (
 	github.com/MarkusFreitag/advent-of-code v0.0.0-20241103012931-4b1e08122711 // indirect
-	github.com/RaphaelPour/stellar v0.0.0-20241124141703-a3f899a9adf3 // indirect
+	github.com/RaphaelPour/stellar v0.0.0-20241202115243-f2ffb866d439 // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
