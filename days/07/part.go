8e109f96b4b85f55ef0f6198f62b23690773342a
Raphael Pour <info@raphaelpour.de>
2024-12-12 06:50:06 +0100

    07: solve

--- days/07/part.go
+++ days/07/part.go
@@ -29,23 +29,17 @@ func part1(data []string) int {
 
 		testResult := sstrings.ToInt(strings.TrimSuffix(fields[0], ":"))
 		numbers := M(strings.Fields(fields[1]), sstrings.ToInt)
-		fmt.Println(numbers)
 
 		found := false
 		for op := 0; op <= smath.Pow(2, len(numbers)-1); op++ {
 			result := numbers[0]
-			fmt.Print(result)
 			for i, num := range numbers[1:] {
 				if op&(1<<(i)) == 0 {
 					result += num
-					fmt.Printf(" + %d", num)
 				} else {
 					result *= num
-					fmt.Printf(" * %d", num)
 				}
 			}
-
-			fmt.Printf("= %d\n", result)
 
 			if result == testResult {
 				found = true
@@ -70,47 +64,31 @@ func Concat(a, b int) int {
 	return sstrings.ToInt(strconv.Itoa(a) + strconv.Itoa(b))
 }
 
-func R(current, result, goal int, path string, numbers []int) bool {
+func R(result, goal int, numbers []int) bool {
 	// result is growing monotonically, if
 	// too big, goal can't be reached anymore
 	if result > goal {
-		fmt.Printf("%s%d=%d (%t)\n", path, current, result, false)
 		return false
 	}
 
 	// no numbers left, goal must be reached or it never will
 	if len(numbers) == 0 {
-		// goal reached \o/
-		fmt.Printf("%s%d=%d (%t)\n", path, current, result, result == goal)
+		// goal reached \o/ ?
 		return result == goal
 	}
 
-	next := -1
-	if len(numbers) > 0 {
-		next = numbers[0]
-	} else {
-		next = current
-	}
-
-	mul := result
-	if path == "" {
-		mul = 1
-	}
-
-	newPath := fmt.Sprintf("%s%d", path, current)
-	if R(next, mul*current, goal, fmt.Sprintf("%s*", newPath), numbers[1:]) {
+	if R(result*numbers[0], goal, numbers[1:]) {
+		return true
+	} else if R(Concat(result, numbers[0]), goal, numbers[1:]) {
 		return true
 	}
-	//else if R(Concat(current, next), result, goal, fmt.Sprintf("%s(%d||%d) ", path, current, next), numbers[1:]) {
-	//	return true
-	//}
 
-	return R(next, result+current, goal, fmt.Sprintf("%s+", newPath), numbers[1:])
+	return R(result+numbers[0], goal, numbers[1:])
 }
 
 func part2(data []string) int {
 	sum := 0
-	for i, line := range data {
+	for _, line := range data {
 		fields := strings.Split(line, ":")
 		if len(fields) != 2 {
 			fmt.Println("unknown field count", len(fields), fields)
@@ -118,12 +96,9 @@ func part2(data []string) int {
 		}
 		testResult := sstrings.ToInt(strings.TrimSuffix(fields[0], ":"))
 		numbers := M(strings.Fields(fields[1]), sstrings.ToInt)
-		fmt.Println(testResult)
-		if R(numbers[0], 0, testResult, "", numbers[1:]) {
+		if R(numbers[0], testResult, numbers[1:]) {
 			sum += testResult
 		}
-		fmt.Println(100.0 / float64(len(data)) * float64(i+1))
-		break
 	}
 	return sum
 }
@@ -132,17 +107,11 @@ func main() {
 	// data := input.LoadString("input")
 	// data := input.LoadDefaultInt()
 	// data := input.LoadInt("input")
-	data := input.LoadString("input_example")
+	data := input.LoadString("input")
 
 	fmt.Println("== [ PART 1 ] ==")
-	// result := part1(data)
-	//if result <= 102900683395 {
-	//	fmt.Printf("%d is too low", result)
-	//} else {
-	//	fmt.Println(result)
-	//}
+	fmt.Println(part1(data))
 
 	fmt.Println("== [ PART 2 ] ==")
 	fmt.Println(part2(data))
-	fmt.Println("too low: 5905560574779")
 }
